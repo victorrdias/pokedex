b304d4a3e0708fe385463a6aceb369c4737951cb
pedr0ferrari <pedrosantanaferrari96@gmail.com>
2022-09-07 18:28:35 -0300

    add pokemonDetail page basics style

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -1,14 +1,19 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import { PokeAPI } from "pokeapi-types";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getPokemonDataById } from "../components/api";
+import { PokemonImage } from "../components/pokemonImage";
+import SearchBar from "../components/SearchBar/searchBar";
+import { colors } from "../lib/pokemonColorsByType";
 
 const PokemonDetail: React.FC = () => {
   const params = useParams();
   const pokemonId = params.id;
 
-  const [pokemon, setPokemon] = useState(null);
+  let navigate = useNavigate();
+
+  const [pokemon, setPokemon] = useState<PokeAPI.Pokemon>(null);
 
   const fetchPokemonData = async (id) => {
     try {
@@ -27,12 +32,44 @@ const PokemonDetail: React.FC = () => {
       setPokemon(pokeData);
     }
   };
+
   useEffect(() => {
     if (pokemonId !== undefined) handleFetchPokemonData(pokemonId);
   }, [pokemonId]);
 
+  if (!pokemon) {
+    return null;
+  }
+
+  const pokeType = pokemon.types.map((type) => type.type.name);
+
+  const firstPokeType = pokeType[0];
+
+  const pokeColor = colors[firstPokeType];
+
   return (
-    <Flex as="main" direction="column" py="4" height="100%">
+    <Flex
+      as="main"
+      direction="column"
+      py="4"
+      height="100%"
+      width="100%"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Button
+        aria-label="back"
+        bgColor="#4873ff"
+        color="black"
+        _hover={{
+          bgColor: "#3665ff",
+        }}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Voltar
+      </Button>
       <Image
         src="/assets/bgpokedex3.jpg"
         zIndex="-1"
@@ -42,7 +79,53 @@ const PokemonDetail: React.FC = () => {
         objectFit="cover"
         top="0"
       />
-      <Text color="white">{pokemon.name}</Text>
+      <Flex
+        direction="column"
+        // boxShadow=" rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;"
+        borderRadius="4"
+        boxSize="12rem"
+        bg={pokeColor}
+        height="70%"
+        width="70%"
+        rounded="3xl"
+        align="center"
+        justify="center"
+        zIndex={2}
+        pb="4"
+        gap="3"
+      >
+        <PokemonImage
+          sprites={pokemon.sprites}
+          name={pokemon.name}
+          boxSize="12rem"
+        />
+      </Flex>
+      <Flex
+        direction="column"
+        boxShadow=" rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;"
+        boxSize="12rem"
+        bg="grey"
+        height="70%"
+        width="70%"
+        rounded="3xl"
+        paddingTop="20"
+        paddingBottom="10"
+        top="-10"
+        position="relative"
+        align="center"
+        justify="center"
+        zIndex={1}
+        gap="2"
+      >
+        <Text color="white">name: {pokemon.name}</Text>
+        <Text color="white">height: {pokemon.height}m</Text>
+        <Text color="white">weight: {pokemon.weight}kg</Text>
+        <Text color="white">HP: {pokemon.stats[0].base_stat}</Text>
+        <Text color="white">attack: {pokemon.stats[1].base_stat}</Text>
+        <Text color="white">defense: {pokemon.stats[2].base_stat}</Text>
+        <Text color="white">special attack: {pokemon.stats[3].base_stat}</Text>
+        <Text color="white">special defense: {pokemon.stats[4].base_stat}</Text>
+      </Flex>
     </Flex>
   );
 };
