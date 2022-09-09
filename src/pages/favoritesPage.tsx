33e8a24de2dea6aa3331a7c3eb1aab8ffa966c15
victorrdias <vi_dias_15@hotmail.com>
2022-09-09 17:18:19 -0300

    create component to fetchFavoritePokemons function

--- src/pages/favoritesPage.tsx
+++ src/pages/favoritesPage.tsx
@@ -1,57 +1,11 @@
-import { Flex, Image } from "@chakra-ui/react";
-import { PokeAPI } from "pokeapi-types";
-import React, { useEffect, useState } from "react";
-import { getPokemonDataByUrl, getPokemons } from "../components/api";
-import PokedexFavorites from "../components/pokedexFavorites";
-import SearchBar from "../components/SearchBar/searchBar";
-import PokemonDetail from "./PokemonDetail";
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import FetchFavoritePokemons from "../components/FetchPokemons/FetchFavoritePokemons";
 
 const FavoritesPage = () => {
-  const [loading, setLoading] = useState(true);
-  const [pokemons, setPokemons] = useState<PokeAPI.Pokemon[] | null>(null);
-
-  const fetchPokemons = async () => {
-    try {
-      const data = await getPokemons();
-      const pokemons = data as PokeAPI.NamedAPIResource[];
-      return pokemons;
-    } catch (error) {
-      console.log("fetchPokemons error: ", error);
-    }
-  };
-
-  const fetchPokemonData = async (url) => {
-    try {
-      const data = await getPokemonDataByUrl(url);
-      const pokemonData = data as PokeAPI.Pokemon;
-      return pokemonData;
-    } catch (error) {}
-  };
-
-  const handleFetchData = async () => {
-    const pokemonList = await fetchPokemons();
-
-    const pokemonDataList = pokemonList.map(async (pokemon) => {
-      const pokemonData = await fetchPokemonData(pokemon.url);
-      return pokemonData;
-    });
-
-    Promise.all(pokemonDataList).then((res) => {
-      setPokemons(res);
-    });
-
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    handleFetchData();
-  }, []);
-
   return (
-    <Flex as="main" direction="column" py="4" height="100%">
-      <SearchBar />
-
-      <PokedexFavorites loading={loading} />
+    <Flex>
+      <FetchFavoritePokemons />;
     </Flex>
   );
 };
